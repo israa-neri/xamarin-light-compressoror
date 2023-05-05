667cbb5c36ea23c783e1dd11f2476f18e1c2092d
Israel Aguilar <israel.aguilar@workwave.com>
2023-05-05 09:13:15 -0400

    Add Light Compressor implementation.

--- video-compress/MainActivity.cs
+++ video-compress/MainActivity.cs
@@ -7,26 +7,23 @@ using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
 using Google.Android.Material.FloatingActionButton;
 using Google.Android.Material.Snackbar;
-using Android.Widget;
-using AndroidX.Core.App;
-using AndroidX.Core.Content;
-using Java.Lang;
-using Android;
-using Java.Net;
-using Org.Apache.Commons.Logging;
+using Com.Abedelazizshe.Lightcompressorlibrary;
+using System.Collections.Generic;
+using Com.Abedelazizshe.Lightcompressorlibrary.Config;
 using Android.Content;
+using Android.Util;
 using Java.IO;
-using Xamarin.Essentials;
-using Iceteck.SiliCompressorrLib;
+using Android.Graphics;
+using Android.Provider;
 
 namespace video_compress
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        public Button btSelect;
-        public VideoView videoView1, videoView2;
-        public TextView textView1, textView2, textView3;
+        private int _selectVideo = 1;
+
+        private String _selectedVideoPath;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -34,38 +31,11 @@ namespace video_compress
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            btSelect = FindViewById<Button>(Resource.Id.bt_select);
-            videoView1 = FindViewById<VideoView>(Resource.Id.video_view1);
-            videoView2 = FindViewById<VideoView>(Resource.Id.Video_view2);
-            textView1 = FindViewById<TextView>(Resource.Id.TextView1);
-            textView2 = FindViewById<TextView>(Resource.Id.text_view2);
-            textView3 = FindViewById<TextView>(Resource.Id.TextView3);
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
 
-            btSelect.Click += (sender, args) =>
-            {
-                // check condition
-                if (ContextCompat.CheckSelfPermission(
-                        this,
-                        Manifest.Permission
-                            .WriteExternalStorage)
-                    == Android.Content.PM.Permission.Granted)
-                {
-                    // When permission is granted
-                    // Create method
-                    selectVideo();
-                }
-                else
-                {
-                    // When permission is not granted
-                    // request permission
-                    ActivityCompat.RequestPermissions(
-                        this,
-                        new string[] {
-                                Manifest.Permission
-                                    .WriteExternalStorage },
-                        1);
-                }
-            };
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -90,6 +60,9 @@ namespace video_compress
             View view = (View) sender;
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (View.IOnClickListener)null).Show();
+
+            Intent i = new Intent(Intent.ActionPick, Android.Provider.MediaStore.Video.Media.ExternalContentUri);
+            StartActivityForResult(i, _selectVideo);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -99,118 +72,73 @@ namespace video_compress
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
-        private void selectVideo()
-        {
-            // Initialize intent
-            Intent intent = new Intent(Intent.Action);
-            // Set type
-            intent.SetType("video/*");
-            // set action
-            intent.SetAction(Intent.ActionGetContent);
-            // Start activity result
-            StartActivityForResult(
-                Intent.CreateChooser(intent, "Select Video"),
-                100);
-        }
-
         protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
-            base.OnActivityResult(requestCode, resultCode,
-                                   data);
-            // Check condition
-            if (requestCode == 100 && resultCode == Result.Ok
-                && data != null)
+            if (resultCode == Result.Ok)
             {
-                // When result is ok
-                // Initialize Uri
-                var uri = data.Data;
-                // Set video uri
-                videoView1.SetVideoURI(uri);
-                // Initialize file
-                File file = new File(
-                    Android.OS.Environment.ExternalStorageDirectory.AbsolutePath);
-                // Create compress video method
-                new CompressVideo(this).Execute(
-                    "false", uri.ToString(), file.Path);
+                if (requestCode == _selectVideo)
+                {
+                    // _selectedVideoPath = getPath(data.Data);
+                    var vcl = new VideoCompressorListener();
+
+                    var configuration = new Configuration(
+                        VideoQuality.Medium,
+                        null,
+                        false,
+                        Java.Lang.Integer.ValueOf(1500000), // Bitrate (bit/s)
+                        false,
+                        false,
+                        Java.Lang.Double.ValueOf(1280),
+                        Java.Lang.Double.ValueOf(720)
+                      );
+
+                    var uris = new List<Android.Net.Uri>
+                      {
+                          data.Data
+                      };
+
+                    VideoCompressor.Start(this,
+                                      uris,
+                                      Android.OS.Environment.DirectoryMovies,
+                                      vcl,
+                                      configuration);
+                    try
+                    {
+                        if (_selectedVideoPath == null)
+                        {
+                            //Log.e("selected video path = null!");
+                            //finish();
+                        }
+                        else
+                        {
+                            /**
+                             * try to do something there
+                             * selectedVideoPath is path to the selected video
+                             */
+                        }
+                    }
+                    catch (IOException e)
+                    {
+                        //#debug
+                        e.PrintStackTrace();
+                    }
+                }
             }
         }
 
-        private class CompressVideo: AsyncTask<string, string, string> {
-
-            private MainActivity _activity;
-            // Initialize dialog
-            Dialog dialog;
-
-            public CompressVideo(MainActivity activity)
-            {
-                _activity = activity;
-            }
-
-            protected override void OnPreExecute()
-            {
-                base.OnPreExecute();
-                // Display dialog
-                dialog = ProgressDialog.Show(
-                    (Platform.CurrentActivity as AppCompatActivity), "", "Compressing...");
-            }
-
-            protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] native_parms)
-            {
-                // Initialize video path
-                string videoPath = null;
-
-                try
-                {
-                    // Initialize uri
-                    Android.Net.Uri uri = Android.Net.Uri.Parse((string)native_parms[1]);
-                    // Compress video
-                    videoPath
-                        = SiliCompressor.With(_activity)
-                              .CompressVideo(uri, (string)native_parms[2]);
-                }
-                catch (URISyntaxException e)
-                {
-                    e.PrintStackTrace();
-                }
-                // Return Video path
-                return videoPath;
-            }
-
-            protected override void OnPostExecute(Java.Lang.Object result)
-            {
-                base.OnPostExecute(result);
-                // Dismiss dialog
-                dialog.Dismiss();
-
-                // Visible all views
-                _activity.videoView1.Visibility = ViewStates.Visible;
-                _activity.textView1.Visibility = ViewStates.Visible;
-                _activity.videoView2.Visibility = ViewStates.Visible;
-                _activity.textView2.Visibility = ViewStates.Visible;
-                _activity.textView3.Visibility = ViewStates.Visible;
-
-                // Initialize file
-                File file = new File((string)result);
-                // Initialize uri
-                Android.Net.Uri uri = Android.Net.Uri.FromFile(file);
-                // set video uri
-                _activity.videoView2.SetVideoURI(uri);
-
-                // start both video
-                _activity.videoView1.Start();
-                _activity.videoView2.Start();
-
-                // Compress video size
-                float size = file.Length() / 1024f;
-                // Set size on text view
-                _activity.textView3.Text = string.Format("Size : %.2f KB", size);
-            }
-
-            protected override string RunInBackground(params string[] @params)
-            {
-                throw new NotImplementedException();
-            }
-        }
+        //public string GetPath(Android.Net.Uri uri)
+        //{
+        //    string[] projection = { MediaStore.Images.Media };
+        //    Cursor cursor = ManagedQuery(uri, projection, null, null, null);
+        //    if (cursor != null)
+        //    {
+        //        int column_index = cursor.GetColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+        //        cursor.MoveToFirst();
+        //        return cursor.GetString(column_index);
+        //    }
+        //    else return null;
+        //}
+    }
 }
-}
+
---
+++ video-compress/VideoCompressorListener.cs
@@ -0,0 +1,53 @@
+﻿using Com.Abedelazizshe.Lightcompressorlibrary;
+
+namespace video_compress
+{
+    public class VideoCompressorListener : Java.Lang.Object, ICompressionListener
+    {
+        public VideoCompressorListener()
+        {
+        }
+
+        public void OnCancelled(int index)
+        {
+            System.Diagnostics.Debug.WriteLine("\n\n");
+            System.Diagnostics.Debug.WriteLine("OnCancelled");
+            System.Diagnostics.Debug.WriteLine(index);
+
+        }
+
+        public void OnFailure(int index, string failureMessage)
+        {
+            System.Diagnostics.Debug.WriteLine("\n\n");
+            System.Diagnostics.Debug.WriteLine("OnFailure");
+            System.Diagnostics.Debug.WriteLine(index);
+            System.Diagnostics.Debug.WriteLine(failureMessage);
+        }
+
+        public void OnProgress(int index, float percent)
+        {
+            System.Diagnostics.Debug.WriteLine("\n\n");
+            System.Diagnostics.Debug.WriteLine("OnProgress");
+            System.Diagnostics.Debug.WriteLine(index);
+            System.Diagnostics.Debug.WriteLine(percent);
+        }
+
+        public void OnStart(int index)
+        {
+            System.Diagnostics.Debug.WriteLine("\n\n");
+            System.Diagnostics.Debug.WriteLine("OnStart");
+            System.Diagnostics.Debug.WriteLine(index);
+        }
+
+        public async void OnSuccess(int index, long size, string path)
+        {
+            System.Diagnostics.Debug.WriteLine("\n\n");
+            System.Diagnostics.Debug.WriteLine("OnSuccess");
+            System.Diagnostics.Debug.WriteLine(index);
+            System.Diagnostics.Debug.WriteLine(size);
+            System.Diagnostics.Debug.WriteLine(path);
+
+        }
+    }
+}
+
